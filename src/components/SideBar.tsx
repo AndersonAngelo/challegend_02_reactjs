52e3d8137846e9e2c42de24af773cb78d5b5606b
Anderson Ângelo <anderson_angelo1@hotmail.com>
2021-12-17 01:51:45 -0300

    componentização do app

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,27 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+import { GenreResponseProps } from '../App';
+
+import '../styles/sidebar.scss';
+
+interface SideBarProps {
+  genres: GenreResponseProps[];
+  selectedGenreId: number;
+  handleClickButton: Function;
+}
+export const SideBar = ({ genres, selectedGenreId, handleClickButton }: SideBarProps) => {
+  return (
+
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+      {genres.map((genre) => (
+        <Button
+          key={String(genre.id)}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => handleClickButton(genre.id)}
+          selected={selectedGenreId === genre.id}
+        />
+      ))}
+    </nav>
+  );
 }
